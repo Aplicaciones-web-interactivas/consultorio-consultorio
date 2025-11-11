9632ae362f479db2018f6e7d0c0be1539971a2b6
gerardocarranco <145299361+gerardocarranco@users.noreply.github.com>
2025-11-11 17:08:57 -0600

    Codigo y migraciones base

---
+++ consultorio/database/migrations/2025_11_11_222804_create_historials_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('historials', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('IdPaciente')->unsigned();
+            $table->foreign('IdPaciente')->references('id')->on('users')->onDelete('cascade');
+            $table->text('Enfermedad');
+            $table->date('Medicacion');
+            $table->string('imagen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('historials');
+    }
+};
