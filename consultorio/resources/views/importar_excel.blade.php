e91980016ec09d9c6e63511da01f0787f1a1e29f
Warvin0 <marvinhazzam0@gmail.com>
2025-11-12 20:33:47 -0600

    Vista reparada y boton hacia importar
    mi vista estaba en tailwind y al parecer cambiaron a boostrap :)

--- consultorio/resources/views/importar_excel.blade.php
+++ consultorio/resources/views/importar_excel.blade.php
@@ -1,56 +1,64 @@
-<x-layouts.app :title="__('Importar Excel')">
-    <div class="col col-span-12 grid grid-cols-1 gap-2 ">
-        <div class="p-6 border rounded-lg shadow-md">
-            <flux:heading size="lg" class="mb-5">Exportar archivo</flux:heading>
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container p-4">
+    <div class="row gy-2">
+
+        <div class="col-12 p-4 border rounded-lg shadow">
+            <h4>Subir archivo:</h4>
             <form method="POST" action="{{ route('import_excel') }}" enctype="multipart/form-data">
                 @csrf
                 <label for="file"></label>
-                <input type="file" name="file" id="file" class="border rounded px-2 py-1 mb-4 w-full" required />
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Subir archivo</button>
+                <input type="file" name="file" id="file" class="form-control mb-4" required accept=".xlsx"/>
+                <button type="submit" class="btn btn-primary">Subir archivo</button>
             </form>
         </div>
-        <div class="p-6 border rounded-lg shadow-md">
-            <flux:heading size="lg" class="mb-5">Subidos:</flux:heading>
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+
+        <div class="col-12 p-4 border rounded-lg shadow">
+            <h4>Subidos:</h4>
+            <table class="table table-hover">
+                <thead class="table-light">
                     <tr>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Apellido</th>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Correo</th>
+                        <th class="text-start small fw-medium text-muted text-uppercase">Nombre</th>
+                        <th class="text-start small fw-medium text-muted text-uppercase">Apellido</th>
+                        <th class="text-start small fw-medium text-muted text-uppercase">Correo</th>
                     </tr>
                 </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
+                <tbody>
                     @foreach($subidos as $user)
                     <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->nombre }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->apellido }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->email}}</td>
+                        <td class="text-nowrap">{{ $user->nombre }}</td>
+                        <td class="text-nowrap">{{ $user->apellido }}</td>
+                        <td class="text-nowrap">{{ $user->email}}</td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
-        <div class="p-6 border rounded-lg shadow-md">
-            <flux:heading size="lg" class="mb-5">Mis Pacientes:</flux:heading>
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+
+        <div class="col-12 p-4 border rounded-lg shadow">
+            <h4>Mis pacientes:</h4>
+            <table class="table table-hover">
+                <thead class="table-light">
                     <tr>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Apellido</th>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Correo</th>
+                        <th class="text-start small fw-medium text-muted text-uppercase">Nombre</th>
+                        <th class="text-start small fw-medium text-muted text-uppercase">Apellido</th>
+                        <th class="text-start small fw-medium text-muted text-uppercase">Correo</th>
                     </tr>
                 </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
+                <tbody>
                     @foreach($mis_pacientes as $paciente)
                     <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $paciente->nombre }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $paciente->apellido }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $paciente->email}}</td>
+                        <td class="text-nowrap">{{ $paciente->nombre }}</td>
+                        <td class="text-nowrap">{{ $paciente->apellido }}</td>
+                        <td class="text-nowrap">{{ $paciente->email}}</td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
+
     </div>
-    
-</x-layouts.app>
+</div> 
+@endsection
